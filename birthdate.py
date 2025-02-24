21d27be6d2c3842b3f9b617978b419a046850289
G5Luffy <vivanbasnyat@gmail.com>
2025-02-24 19:23:58 +0545

    asd

---
+++ birthdate.py
@@ -0,0 +1,14 @@
+dadbdaydate= 16
+dadbdaymonth= "january"
+print("birthday date is", dadbdaydate)
+print("birthday month is", dadbdaymonth)
+mombdaydate= 18
+mombdaymonth= "march"
+print("birthday date is", mombdaydate)
+print("birthday month is", mombdaymonth)
+brotherbdaydate= 11
+brotherbdaymonth= "september"
+brotherbdayyear= "2021"
+print("birthday date is", brotherbdaydate)
+print("birthday date is", brotherbdaymonth)
+print("birthday year is", brotherbdayyear)
