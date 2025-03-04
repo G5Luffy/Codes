2d8f576ed889dc0c047a0877fdc9172b8d553bc7
G5Luffy <vivanbasnyat@gmail.com>
2025-03-04 19:51:24 +0545

    commit

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+name = "Bird"
+age = 15
+is_student = True
+weight = 38.5
+print("name :", name)
+print("Data type of name is", type(name))
+print("age :", age)
+print("Data type of name is", type(age))
+print("is_student :", is_student)
+print("Data type of is_student", type(is_student))
+print("weight :", weight)
+print("Data type of weight is", type(weight))
+print("\n After Typecasting.....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data type of weight is", type(weight))
